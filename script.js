5f455e05068791cb896b1c7829b31260e639110b
dhrumi-vaidya <dvtheta@gmail.com>
2025-03-12 11:10:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
-var table = document.getElementById("table");
-const submitbtn = document.getElementById("buttonsubmit");
-const refreshbtn = document.getElementById("buttonrefresh");
-const result = document.getElementById("result");
-submitbtn.addEventListener("click", () => {
+document.getElementById("form").addEventListener("submit", (event) => {
   event.preventDefault();
-  // const weight = document.getElementById("weight").value;
-  // const height = document.getElementById("Height").value;
+  calculateBMI();
 });
+
+const result = document.getElementById("result");
+
+const refreshbtn = document.getElementById("buttonrefresh");
 refreshbtn.addEventListener("click", () => {
-  window.location.reload();
+  result.innerHTML = "";
 });
-
 function calculateBMI() {
-  const weight = document.getElementById("weight").value;
-  const height = document.getElementById("Height").value;
+  const weight = parseFloat(document.getElementById("weight").value);
+  const height = parseFloat(document.getElementById("height").value);
 
-  if (weight <= 0 || height <= 0) {
-    alert("Enter a value greater than 0");
-  } else {
+  if (weight >= 5 && weight <= 640 && height >= 54 && height <= 280) {
     let resulttext;
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    let category = "";
 
-    var row = table.insertRow(4);
-    var cell1 = row.insertCell(0);
-
-    cell1.innerHTML = `BMI : ${bmi}`;
-
-    if (bmi < 16) result.innerHTML = "Severe Thinness";
-    else if (bmi >= 16 && bmi < 17) result.innerHTML = "Moderate Thinness";
-    else if (bmi >= 17 && bmi < 18.5) result.innerHTML = "Mild Thinness";
-    else if (bmi >= 18.5 && bmi < 25) result.innerHTML = "Normal";
-    else if (bmi >= 25 && bmi < 30) result.innerHTML = "overweight";
-    else if (bmi >= 30 && bmi < 35) result.innerHTML = "Class 1 Obese";
-    else if (bmi >= 35 && bmi < 40) result.innerHTML = "Class 2 Obese";
-    else result.innerHTML = "Severe Obacity ";
+    if (bmi < 16) category = "Severe Thinness";
+    else if (bmi >= 16 && bmi < 17) category = "Moderate Thinness";
+    else if (bmi >= 17 && bmi < 18.5) category = "Mild Thinness";
+    else if (bmi >= 18.5 && bmi < 25) category = "Normal";
+    else if (bmi >= 25 && bmi < 30) category = "overweight";
+    else if (bmi >= 30 && bmi < 35) category = "Class 1 Obese";
+    else if (bmi >= 35 && bmi < 40) category = "Class 2 Obese";
+    else category = "Severe Obacity ";
+    result.innerHTML = `<pre>BMI: ${bmi} <strong> ${category} </strong></pre>`;
+  }
+  else {
+    alert("Enter a valid value within the given range");
   }
 }
